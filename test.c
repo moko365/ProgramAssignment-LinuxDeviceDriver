b790bb5256928261803153dae4358481d5e3d149
android <android@mokoid-lab.(none)>
2012-02-11 16:12:26 +0800

    Add test.c user program

---
+++ test.c
@@ -0,0 +1,17 @@
+/*
+ * Filename: test.c
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+
+int main(void)
+{
+    int fd;
+
+    fd = open("/dev/cdata", O_RDWR);
+    close(fd);
+}
