39058aaa1a0994f5a2fa672c61e994a55403ef60
android <android@mokoid-lab.(none)>
2012-02-11 15:17:37 +0800

    Register a character driver

--- cdata.c
+++ cdata.c
@@ -20,16 +20,35 @@ static int cdata_open(struct inode *inode, struct file *filp)
 	return 0;
 }
 
+ssize_t cdata_write(struct file *filp, const char *buf, size_t size, 
+			loff_t *off)
+{
+	return 0;
+}
+
+int cdata_close(struct inode *inode, struct file *filp)
+{
+	return 0;
+}
+
 static struct file_operations cdata_fops = {	
 	open:		cdata_open,
+	release:	cdata_close,
+	write:		cdata_write,
 };
 
 int cdata_init_module(void)
 {
+	if (register_chrdev(121, "cdata", &cdata_fops) < 0) {
+	    printk(KERN_INFO "CDATA: can't register driver\n");
+	    return -1;
+	}
+	return 0;
 }
 
 void cdata_cleanup_module(void)
 {
+	unregister_chrdev(121, "cdata");
 }
 
 module_init(cdata_init_module);
