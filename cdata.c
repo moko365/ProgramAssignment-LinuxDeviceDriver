28c49ac5c4337133667e6525f222a3f49302fd1a
android <android@mokoid-lab.(none)>
2012-02-12 10:13:53 +0800

    Minor number and THIS_MODULE

--- cdata.c
+++ cdata.c
@@ -17,8 +17,19 @@
 
 static int cdata_open(struct inode *inode, struct file *filp)
 {
+	int i;
+	int minor;
+
 	printk(KERN_INFO "CDATA: in open\n");
+
+	minor = MINOR(inode->i_rdev);
+	printk(KERN_INFO "CDATA: minor = %d\n", minor);
+
 	return 0;
+}
+
+ssize_t cdata_read(struct file *filp, char *buf, size_t size, loff_t *off)
+{
 }
 
 ssize_t cdata_write(struct file *filp, const char *buf, size_t size, 
@@ -32,10 +43,19 @@ int cdata_close(struct inode *inode, struct file *filp)
 	return 0;
 }
 
+int cdata_ioctl(struct inode *inode, struct file *filp, 
+			unsigned int cmd, unsigned long arg)
+{
+}
+
 static struct file_operations cdata_fops = {	
+	owner:		THIS_MODULE,
 	open:		cdata_open,
 	release:	cdata_close,
+	read:		cdata_read,
 	write:		cdata_write,
+	ioctl:		cdata_ioctl,
+
 };
 
 int cdata_init_module(void)
