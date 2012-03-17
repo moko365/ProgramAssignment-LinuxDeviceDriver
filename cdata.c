781b1cfb0edd61768fd837eadeb3a8d212666171
android <android@mokoid-lab.(none)>
2012-03-17 14:01:51 +0800

    Modify: disable local IRQ

--- cdata.c
+++ cdata.c
@@ -128,10 +128,10 @@ static ssize_t cdata_write(struct file *filp, const char *buf, size_t size,
 
 	down_interruptible(&cdata->sem);
 
-	spin_lock()
+	spin_lock_irqsave(&cdata->lock);
 	pixel = cdata->buf;
 	index = cdata->index;
-	spin_unlock()
+	spin_unlock_irqsave(&cdata->lock);
 
 	timer = &cdata->flush_timer;
 	sched = &cdata->sched_timer;
