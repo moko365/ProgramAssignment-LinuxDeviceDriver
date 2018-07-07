4e5aef2ca26f247c48ec79d766d986edbd855e60
jollen <jollen@jollen.org>
2018-07-07 03:13:29 +0000

    update cdata.c

--- cdata.c
+++ cdata.c
@@ -12,51 +12,59 @@
 #include <linux/irq.h>
 #include <linux/miscdevice.h>
 #include <linux/input.h>
-#include <linux/semaphore.h>
+#include <linux/slab.h>
+#include <linux/platform_device.h>
+#include <linux/debugfs.h>
 #include <linux/spinlock.h>
+#include <linux/workqueue.h>
 #include <asm/io.h>
 #include <asm/uaccess.h>
+
 #include "cdata_ioctl.h"
 
-#define	BUF_SIZE	(128)
-#define	LCD_SIZE	(320*240*4)
+#define CDATA_MAJOR 121
+#define	BUF_SIZE 64
+
+#ifdef __USE_FBMEM__
+#define FRAMEBUFFER_SIZE (640*480*1)
+static unsigned int framebuffer_off;
+#endif
+
+static DEFINE_MUTEX(ioctl_lock);
+static struct dentry *debugfs;
+
+void *write_framebuffer_with_timer(unsigned long);
+void write_framebuffer_with_work(struct work_struct *);
 
 struct cdata_t {
-    unsigned long *fb;
-    unsigned char *buf;
-    unsigned int  index;
-    unsigned int  offset;
-
-    struct timer_list	flush_timer;
-    struct timer_list	sched_timer;
-
-    wait_queue_head_t	wq;
-    struct semaphore 	sem;
-    spinlock_t		lock;
+	unsigned char buf[BUF_SIZE];
+	int idx;
+	wait_queue_head_t writeable;
+	struct timer_list timer;
+	struct work_struct work;
+	struct mutex write_lock;
+	spinlock_t lock;
+#ifdef __USE_FBMEM__
+	unsigned char *iomem;
+#endif
 };
 
 static int cdata_open(struct inode *inode, struct file *filp)
 {
-	int minor;
 	struct cdata_t *cdata;
 
-	printk(KERN_INFO "CDATA: in open\n");
+	printk(KERN_ALERT "cdata in open: filp = %p\n", filp);
 
-	minor = MINOR(inode->i_rdev);
-	printk(KERN_INFO "CDATA: minor = %d\n", minor);
+	cdata = kzalloc(sizeof(*cdata), GFP_KERNEL);
+	cdata->idx = 0;
+#ifdef __USE_FBMEM__
+	cdata->iomem = ioremap(0xe0000000, FRAMEBUFFER_SIZE);
+#endif
 
-	cdata= kmalloc(sizeof(struct cdata_t), GFP_KERNEL);
-	cdata->buf = kmalloc(BUF_SIZE, GFP_KERNEL);
-	cdata->fb = ioremap(0x33f00000, 320*240*4);
-	cdata->index = 0;
-	cdata->offset = 0;
-
-	init_timer(&cdata->flush_timer);
-	init_timer(&cdata->sched_timer);
-
-	init_waitqueue_head(&cdata->wq);
-
-	sema_init(&cdata->sem, 1);
+	init_waitqueue_head(&cdata->writeable);
+	init_timer(&cdata->timer);
+	INIT_WORK(&cdata->work, write_framebuffer_with_work);
+	mutex_init(&cdata->write_lock);
 	spin_lock_init(&cdata->lock);
 
 	filp->private_data = (void *)cdata;
@@ -64,217 +72,239 @@ static int cdata_open(struct inode *inode, struct file *filp)
 	return 0;
 }
 
-static ssize_t cdata_read(struct file *filp, char *buf, size_t size, loff_t *off)
-{
-	return 0;
-}
-
-void flush_lcd(unsigned long priv)
-{
-	struct cdata_t *cdata = (struct cdata *)priv;
-	unsigned char *fb;
-	unsigned char *pixel;
-	int index;
-	int offset;
-	int i;
-	int j;
-
-	spin_lock_irqsave(&cdata->lock);
-	fb = (unsigned char *)cdata->fb;
-	pixel = cdata->buf;
-	index = cdata->index;
-	offset = cdata->offset;
-	spin_unlock_irqsave(&cdata->lock);
-
-	for (i = 0; i < index; i++) {
-	    writeb(pixel[i], fb+offset);
-	    offset++;
-	    if (offset >= LCD_SIZE)
-		offset = 0;
-	    // Lab
-	    for (j = 0; j < 100000; j++);
-	}
-	
-	cdata->index = 0;
-	cdata->offset = offset;
-}
-
-void cdata_wake_up(unsigned long priv)
-{
-	struct cdata_t *cdata = (struct cdata *)priv;
-	struct timer_list *sched;
-        wait_queue_head_t	*wq;
-
-	sched = &cdata->sched_timer;
-	wq = &cdata->wq;
-
-	wake_up(wq);
-
-	sched->expires = jiffies + 10;
-	add_timer(sched);
-}
-
-static ssize_t cdata_write(struct file *filp, const char *buf, size_t size, 
-			loff_t *off)
-{
-	struct cdata_t *cdata = (struct cdata *)filp->private_data;
-	struct timer_list *timer;
-	struct timer_list *sched;
-	unsigned char *pixel;
-	unsigned int index;
-	unsigned int i;
-        wait_queue_head_t *wq;
-	wait_queue_t wait;
-
-	down_interruptible(&cdata->sem);
-
-	spin_lock_irqsave(&cdata->lock);
-	pixel = cdata->buf;
-	index = cdata->index;
-	spin_unlock_irqsave(&cdata->lock);
-
-	timer = &cdata->flush_timer;
-	sched = &cdata->sched_timer;
-	wq = &cdata->wq;
-
-	up(&cdata->sem);
-
-	for (i = 0; i < size; i++) {
-	    if (index >= BUF_SIZE) {
-		
-		down_interruptible(&cdata->sem);
-		cdata->index = index;
-		up(&cdata->sem);
-
-		timer->expires = jiffies + 5*HZ;
-		timer->function = flush_lcd;
-		timer->data = (unsigned long)cdata;
-		add_timer(timer);
-
-		sched->expires = jiffies + 10;
-		sched->function = cdata_wake_up;
-		sched->data = (unsigned long)cdata;
-		add_timer(sched);
-
-		wait.flags = 0;	
-		wait.task = current;
-		add_wait_queue(wq, &wait);
-repeat:
- 		current->state = TASK_INTERRUPTIBLE;
-		smp_mb();
-		schedule();
- 		
-		down_interruptible(&cdata->sem);
-		index = cdata->index;
-		up(&cdata->sem);
-
-		if (index != 0)
-		    goto repeat;
-
-		remove_wait_queue(wq, &wait);
-		del_timer(sched);
-	    }
-	    copy_from_user(&pixel[index], &buf[i], 1);
-	    index++;
-	}
-
-	down_interruptible(&cdata->sem);
-    	cdata->index = index;
-	up(&cdata->sem);
-
-	return 0;
-}
-
 static int cdata_close(struct inode *inode, struct file *filp)
 {
-	struct cdata_t *cdata = (struct cdata *)filp->private_data;
-	
-	flush_lcd((unsigned long)cdata);
-
-	del_timer(&cdata->flush_timer);
-
-	kfree(cdata->buf);
-	kfree(cdata);
-
-	return 0;
-}
-
-static int cdata_ioctl(struct inode *inode, struct file *filp, 
-		unsigned int cmd, unsigned long arg)
-{
-	struct cdata_t *cdata = (struct cdata *)filp->private_data;
-	int n;
-	unsigned long *fb;
+	struct cdata_t *cdata = (struct cdata_t *)filp->private_data;
+	int idx;
 	int i;
 
-	switch (cmd) {
-	    case CDATA_CLEAR:
-		printk(KERN_INFO "CDATA_CLEAR: %d pixel\n", n);
-		return 0;
+	idx = cdata->idx;
+
+	for (i = 0; i < idx; i++) {
+		printk(KERN_ALERT "buf[%d]: %c\n", i, cdata->buf[i]);
 	}
 
-	return -ENOTTY;
+	del_timer(&cdata->timer);
+	kfree(cdata);
+	
+	return 0;
 }
 
-int cdata_mmap(struct file *filp, struct vm_area_struct *vma)
+static ssize_t cdata_read(struct file *filp, const char __user *user, 
+	size_t size, loff_t *off)
 {
-	unsigned long from;
-	unsigned long to;
-	unsigned long size;
+	printk(KERN_ALERT "cdata in read\n");
+	return 0;
+}
 
-	from = vma->vm_start;
-	to = 0x33f00000;
-	size = vma->vm_end-vma->vm_start;
+void write_framebuffer_with_work(struct work_struct *work)
+{
+	struct cdata_t *cdata = container_of(work, struct cdata_t, work);
 
-	while (size) {
-	    remap_pfn_range(vma, from, 
-                     0x33f00000, 
-                     PAGE_SIZE, vma->vm_page_prot);
+	cdata->idx = 0;
+	wake_up_interruptible(&cdata->writeable);
+}
 
-	    from += PAGE_SIZE;
-	    to += PAGE_SIZE;
-	    size -= PAGE_SIZE;
+void *write_framebuffer_with_timer(unsigned long arg)
+{
+	struct cdata_t *cdata = (struct cdata_t *)arg;
+	int i;
+
+#ifdef __USE_FBMEM__
+	unsigned char *iomem;
+	iomem = cdata->iomem;
+
+	for (i = 0; i < BUF_SIZE - 1; i++) {
+		if (framebuffer_off >= FRAMEBUFFER_SIZE)
+			framebuffer_off = 0;
+		writeb(cdata->buf[i], iomem + framebuffer_off);
+		framebuffer_off++;
+	}
+#endif
+	cdata->idx = 0;
+	wake_up_interruptible(&cdata->writeable);
+}
+
+static ssize_t cdata_write(struct file *filp, const char __user *user, 
+	size_t size, loff_t *off)
+{
+	struct cdata_t *cdata = (struct cdata_t *)filp->private_data;
+	DECLARE_WAITQUEUE(wait, current);
+	struct timer_list *timer;
+	int i;
+	int idx;
+
+	mutex_lock_interruptible(&cdata->write_lock);
+	idx = cdata->idx;
+	timer = &cdata->timer;
+
+	for (i = 0; i < size; i++) {
+		if (idx > (BUF_SIZE - 1)) {
+repeat:
+			add_wait_queue(&cdata->writeable, &wait);
+			current->state = TASK_INTERRUPTIBLE;
+
+			schedule_work(&cdata->work);
+			mutex_unlock(&cdata->write_lock);
+
+			schedule();
+
+			remove_wait_queue(&cdata->writeable, &wait);
+			idx = cdata->idx;
+			if (idx <= (BUF_SIZE - 1))
+				break;
+			
+			printk(KERN_ALERT "race condition: idx = %d\n", idx);	
+			mutex_lock_interruptible(&cdata->write_lock);
+			goto repeat;
+		}
+		copy_from_user(&cdata->buf[idx], &user[i], 1);
+		idx++;
 	}
 
-	printk(KERN_ALERT "vma start: %p\n", vma->vm_start);
-	printk(KERN_ALERT "vma end: %p\n", vma->vm_end);
+	cdata->idx = idx;
+	mutex_unlock(&cdata->write_lock);
 
 	return 0;
 }
 
-static struct file_operations cdata_fops = {	
-	owner:		THIS_MODULE,
-	open:		cdata_open,
-	release:	cdata_close,
-	read:		cdata_read,
-	write:		cdata_write,
-	ioctl:		cdata_ioctl,
-	mmap:		cdata_mmap,
+static long cdata_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	struct cdata_t *cdata = (struct cdata_t *)filp->private_data;
+	char *buf;
+	int idx;
+	int i;
+	int ret = 0;
+	char *user;
+	int size;
+
+	if (mutex_lock_interruptible(&ioctl_lock))
+		return -EINTR;
+
+	user = (char *)arg;
+	size = sizeof(*user);
+
+	idx = cdata->idx;
+	buf = cdata->buf;
+
+	switch (cmd) {
+	case IOCTL_EMPTY:
+		idx = 0;
+		break;
+	case IOCTL_SYNC:
+		printk(KERN_ALERT "in ioctl: %s\n", buf);
+		break;
+	case IOCTL_NAME:
+		for (i = 0; i < size; i++) {
+			if (idx > (BUF_SIZE - 1)) {
+				ret = -EFAULT;
+				goto exit;
+			}
+			copy_from_user(&buf[idx], &user[i], 1);
+			idx++;
+		}
+		break;
+	default:
+		goto exit;
+	}
+
+exit:
+	cdata->idx = idx;
+	mutex_unlock(&ioctl_lock);
+	return ret;
+}
+
+static int cdata_mmap(struct file *filp, struct vm_area_struct *vma)
+{
+    unsigned long start = vma->vm_start;
+    unsigned long end = vma->vm_end;
+    unsigned long size = end - start;
+
+    printk(KERN_ALERT "remap %p to 0xe0000000\n", start);
+    remap_pfn_range(vma, start, 0xe0000000, size, PAGE_SHARED);
+
+    return 0;
+}
+
+static struct file_operations cdata_fops = {
+    owner:      	THIS_MODULE,
+    open:		cdata_open,
+    read:		cdata_read,
+    write:		cdata_write,
+    mmap:		cdata_mmap,
+    unlocked_ioctl:	cdata_ioctl,
+    release:    	cdata_close
+};
+
+static struct miscdevice cdata_miscdev = {
+	.minor	= 77,
+	.name	= "cdata-misc",
+	.fops	= &cdata_fops,
+};
+
+static int cdata_plat_probe(struct platform_device *pdev)
+{
+	int ret = 0;
+
+	ret = misc_register(&cdata_miscdev);
+	if (ret < 0) {
+		printk(KERN_ALERT "misc_register failed\n");
+		goto exit;
+	}
+
+	printk(KERN_ALERT "cdata module: registered!\n");
+
+exit:
+	return ret;
+}
+
+static int cdata_plat_remove(struct platform_device *pdev)
+{
+	misc_deregister(&cdata_miscdev);
+	printk(KERN_ALERT "cdata module: unregisterd.\n");
+}
+
+static struct platform_driver cdata_plat_driver = {
+	.probe 			= cdata_plat_probe,
+	.remove 		= cdata_plat_remove,
+	.driver 		= {
+		   .name	= "cdata",
+		   .owner	= THIS_MODULE,
+	},
 };
 
 int cdata_init_module(void)
 {
-	unsigned long *fb;
-	int i;
+	int ret = 0;
 
-	fb = ioremap(0x33f00000, 320*240*4);
-	for (i = 0; i < 320*240; i++)
-		writel(0x00ff0000, fb++);
-	
-	if (register_chrdev(121, "cdata", &cdata_fops) < 0) {
-	    printk(KERN_INFO "CDATA: can't register driver\n");
-	    return -1;
+#ifdef __USE_FBMEM__
+	framebuffer_off = 0;
+#endif
+	debugfs = debugfs_create_file("cdata", S_IRUGO, NULL, NULL, &cdata_fops);
+
+	if (IS_ERR(debugfs)) {
+		ret = PTR_ERR(debugfs);
+		printk(KERN_ALERT "debugfs_create_file failed\n");
+		goto exit;
 	}
-	printk(KERN_INFO "CDATA: cdata_init_module\n");
-	return 0;
+
+	printk(KERN_ALERT "cdata: debugfs created\n");
+
+	mutex_init(&ioctl_lock);
+
+	ret = platform_driver_register(&cdata_plat_driver);
+exit:
+	return ret;
 }
 
 void cdata_cleanup_module(void)
 {
-	unregister_chrdev(121, "cdata");
+	platform_driver_unregister(&cdata_plat_driver);
+	debugfs_remove(debugfs);
 }
 
 module_init(cdata_init_module);
 module_exit(cdata_cleanup_module);
 
 MODULE_LICENSE("GPL");
+
